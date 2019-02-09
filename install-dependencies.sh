b96f4c529e60af2e8cf093869d0e5321698388d1
Terkel Gjervig <terkel@terkel.com>
2019-02-09 18:12:29 +0100

    Added script to install dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+npm install -g javascript-typescript-langserver   # language server used for js-cleverness
+npm install -g neovim                             # node neovim client
+npm install -g typescript                         # some people like types in their JS
